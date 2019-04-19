44dd3632d13a5b044c8eab44ebab765e5cacd925
Mazdah <woohj70@gmail.com>
2019-04-19 16:23:33 +0900

    Initial Commit

---
+++ Tistoria/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tistoria
+//
+//  Created by Mazdah on 19/04/2019.
+//  Copyright © 2019 Mazdah.com. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
